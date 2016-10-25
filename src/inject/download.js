865e99a02530637f5228fb7159310203ff8a51db
ShaneOH <shane.ohanlon93@gmail.com>
2016-10-24 22:55:03 -0400

    Add progress percentage

--- src/inject/download.js
+++ src/inject/download.js
@@ -1,5 +1,10 @@
 const downloadPlaylist = () => {
 
+	const progress = document.createElement('p')
+		progress.innerHTML = "Downloading ... 0%";
+	const download_button = document.getElementById('download_button');
+		download_button.insertAdjacentElement('afterend', progress);
+
 	const tracks = document.getElementsByClassName('fap-single-track');
 	const title = document.getElementsByTagName("title")[0].innerHTML;
 	const zipFileName = title.substr(0, title.indexOf('|')).trim();
@@ -17,6 +22,7 @@ const downloadPlaylist = () => {
 		}
 	}
 
+	let i = 0;
 	urls.reduce((prev, url) => {
 
 		return prev.then(() => {
@@ -25,18 +31,24 @@ const downloadPlaylist = () => {
 							.then((res) => res.blob())
 							.catch((err) => console.log("Error: ", err));
 
+			i++;
+			const percent = Math.round((i / urls.length) * 100);
+			progress.innerHTML = "Downloading ... " + percent + "%";
 			zip.file(songFileName, promise);
 
 			return promise;
 		})
 
 	}, Promise.resolve())
-		.then(() => zip.generateAsync({type:"blob"}))
-	 	.then((blob) => saveAs(blob, zipFileName));
+		.then(() => zip.generateAsync({type:"blob"},
+			(metadata) => progress.innerHTML = "Zipping ... " + Math.round(metadata.percent) + "%"))
+	 	.then((blob) => saveAs(blob, zipFileName))
+		.then(() => progress.innerHTML = "Done! Please support the artists that you enjoy :)");
 }
 
 const download_button = document.createElement('a');
-	download_button.innerHTML = '<a href="#">Download Playlist</a>';
+	download_button.id = "download_button";
+	download_button.innerHTML = '<a href="">Download Playlist</a>';
 	download_button.addEventListener('click', downloadPlaylist)
 
 const tracklist = document.getElementById('tracklist');
