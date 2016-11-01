42bd0bb233674441ab59dff2158a88b9e608eeb4
ShaneOH <shane.ohanlon93@gmail.com>
2016-10-31 23:46:02 -0400

    download individual tracks

--- src/inject/download.js
+++ src/inject/download.js
@@ -1,25 +1,29 @@
+const getUrl = (track => {
+	const encodedUrl = track.getAttribute('href')
+
+	try {
+		return atob(encodedUrl);
+	}
+	catch (e) {
+		return '';
+	}
+});
+
 const downloadPlaylist = () => {
 
 	const progress = document.createElement('p')
 		progress.innerHTML = "Downloading ... 0%";
-	const download_button = document.getElementById('download_button');
-		download_button.insertAdjacentElement('afterend', progress);
+	const download_playlist_button = document.getElementById('download_playlist_button');
+		download_playlist_button.insertAdjacentElement('afterend', progress);
 
-	const tracks = document.getElementsByClassName('fap-single-track');
 	const title = document.getElementsByTagName("title")[0].innerHTML;
 	const zipFileName = title.substr(0, title.indexOf('|')).trim();
 	const urls = [];
 	const zip = new JSZip();
 
 	for (const track of tracks) {
-		const encodedUrl = track.getAttribute('href')
-
-		try {
-			urls.push(atob(encodedUrl));
-		}
-		catch (e) {
-			continue;
-		}
+		url = getUrl(track);
+		if (url !== '') urls.push(url);
 	}
 
 	let i = 0;
@@ -50,10 +54,25 @@ const downloadPlaylist = () => {
 		.then(() => progress.innerHTML = "Done! Please support the artists that you enjoy :)");
 }
 
-const download_button = document.createElement('a');
-	download_button.id = "download_button";
-	download_button.innerHTML = '<a href="">Download Playlist</a>';
-	download_button.addEventListener('click', downloadPlaylist)
+const tracks = document.getElementsByClassName('fap-single-track');
+for (const track of tracks) {
+	const url = getUrl(track);
+	if (url !== '') {
+		const download_track_button = document.createElement('a');
+			download_track_button.href = url;
+			download_track_button.download = track.title;
+			download_track_button.target = '_blank';
+			download_track_button.textContent = 'Download Track'
+			download_track_button.style.color = 'grey';
+			download_track_button.style.paddingLeft = '10px';
+			track.insertAdjacentElement('afterend', download_track_button);
+	}
+}
+
+const download_playlist_button = document.createElement('a');
+	download_playlist_button.id = "download_playlist_button";
+	download_playlist_button.innerHTML = '<a href="">Download Playlist</a>';
+	download_playlist_button.addEventListener('click', downloadPlaylist)
 
 const tracklist = document.getElementById('tracklist');
-	tracklist.insertAdjacentElement('afterbegin', download_button);
+	tracklist.insertAdjacentElement('afterbegin', download_playlist_button);
